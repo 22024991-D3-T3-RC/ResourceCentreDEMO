9e8e373edc05eed696f46c2ea1cb83ca4d1a8032
22023590 <22023590@manfred>
2023-07-14 17:24:27 +0800

    return

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -254,7 +254,7 @@ public class ResourceCentre {
 
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList, String tag) {
 		boolean isReturned = false;
-		// write your code here
+		// write your code here p
 
 		for (int i = 0; i < chromebookList.size(); i++) {
 			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
